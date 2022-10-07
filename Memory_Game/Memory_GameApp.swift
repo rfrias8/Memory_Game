3c32df0a46806c579078942cc3b14f2689507db5
rfrias8 <rfrias8@gmail.com>
2022-10-06 23:14:33 -0500

    Initial Commit

---
+++ Memory_Game/Memory_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Memory_GameApp.swift
+//  Memory_Game
+//
+//  Created by Rita Frias on 10/6/22.
+//
+
+import SwiftUI
+
+@main
+struct Memory_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
