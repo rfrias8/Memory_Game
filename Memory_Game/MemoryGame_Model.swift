537f0e4957f699186be33906cff1b3179d06f2f1
rfrias8 <rfrias8@gmail.com>
2022-10-07 19:39:28 -0500

    Added init to Model to create Pairs and append it to array

--- Memory_Game/MemoryGame_Model.swift
+++ Memory_Game/MemoryGame_Model.swift
@@ -9,15 +9,25 @@
 import Foundation
 
 struct MemoryGame<CardIcon>{
-    var cards: Array<Card>
+    private(set) var cards: Array<Card>
     
     func chooseCard(_ card: Card) {//external name, internal name,
         
     }
     
+    init(cardPairsCount: Int, createCardContent: (Int) -> CardIcon) {
+        cards = Array<Card>()
+        //This for loop will take the num of cards and multiply by 2 and add them to the array
+        for indexOfPair in 0..<cardPairsCount {
+            let content: CardIcon = createCardContent(indexOfPair)
+            cards.append(Card(icon:content))
+            cards.append(Card(icon:content))
+        }
+    }
+    
     struct Card {//This is technically MemoryGame.Card since it is inside another struct
-        var faceUp: Bool
-        var isMatch: Bool
+        var faceUp: Bool = false
+        var isMatch: Bool = false
         var icon: CardIcon
     }
 }
